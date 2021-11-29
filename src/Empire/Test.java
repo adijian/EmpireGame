e43e4762c06c5c7e9c308fa15076702b637be7b4
CORP\adij <adij@nayax.com>
2021-11-29 20:59:05 +0200

    office1

--- src/Empire/Test.java
+++ src/Empire/Test.java
@@ -1,2 +1,37 @@
-package Empire;public class Test {
+package Empire;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Test {
+    public static List<Villager> villagersArray = new ArrayList<>();
+    public static List<Villager> villagersWoodcuttingArray = new ArrayList<>();
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        Villager villager = new Villager();
+        villager.job = "job";
+        System.out.println(Test.arrayManipulation());
+    }
+
+    public static int arrayManipulation(){
+        for (Villager d : Test.villagersArray){
+            if(d.getJob() != null && d.getJob().contains("")){
+
+            }
+            //something here
+        }
+        return Test.villagersWoodcuttingArray.size();
+    }
+
+    static class Villager {
+        int number;
+        int hp = 20;
+        String name;
+        String job = " ";
+        public String getJob() {
+            return this.job;
+        }
+    }
 }
