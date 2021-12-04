fb933d6a37c398837456fd8e94c134d56edafae2
adijian <shoogeh.jian@gmail.com>
2021-12-04 21:10:00 +0200

    2

--- src/Empire/GUI.java
+++ src/Empire/GUI.java
@@ -5,11 +5,12 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class GUI {
-    private static JButton villagersButton;
+
     public static void main(String[] args) {
 //        Colors
         Color frameColor = new Color(36, 91, 101);
@@ -17,18 +18,21 @@ public class GUI {
 
 //        Frame and display
         JFrame frame = new JFrame();
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setSize(1920,1080);
+//        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setSize(750,450);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.getContentPane().setBackground(frameColor);
+        Image icon = Toolkit.getDefaultToolkit().getImage("C:/JavaProject/EmpireGame/out/production/EmpireGame/Images/icon.png");
+        frame.setIconImage(icon);
+        frame.setTitle("EmpireGame - GUI Demo");
 
 //        Panel for currencies
         JPanel panelCurrencies = new JPanel(new BorderLayout());
         panelCurrencies.setBackground(frameColor);
 
 //        Villagers tab
-        ImageIcon villagersTab = new ImageIcon("image1.png");
+        ImageIcon villagersTab = new ImageIcon("C:/JavaProject/EmpireGame/src/Images/Raw/image1.png");
         Image image1 = villagersTab.getImage(); // transform it
 //        Image newimg1 = image1.getScaledInstance((int) (350/1.5), (int) (200/1.5),  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
         Image newimg1 = image1.getScaledInstance((int) (350*2), (int) (200*2),  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
@@ -42,9 +46,9 @@ public class GUI {
 
 //        Villagers Button
 
-        villagersButton = new JButton();
+        JButton villagersButton = new JButton();
         villagersButton.setBounds(475,208,(int) (148*1.5),(int) (131*1.5));
-        ImageIcon villagersTabButtonIcon = new ImageIcon("button.png");
+        ImageIcon villagersTabButtonIcon = new ImageIcon("C:/JavaProject/EmpireGame/src/Images/Raw/button.png");
         Image imagevillagersTabButtonIcon = villagersTabButtonIcon.getImage(); // transform it
         Image newimgvillagersTabButtonIcon = imagevillagersTabButtonIcon.getScaledInstance((int) (148*1.5), (int) (131*1.5),  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
         villagersTabButtonIcon = new ImageIcon(newimgvillagersTabButtonIcon);  // transform it back
@@ -56,7 +60,7 @@ public class GUI {
         villagersButton.addActionListener(e -> {
             System.out.println("A villager was born");
 
-            ImageIcon villagersTabButtonIconPressed = new ImageIcon("button1.png");
+            ImageIcon villagersTabButtonIconPressed = new ImageIcon("C:/JavaProject/EmpireGame/src/Images/Raw/button1.png");
             Image imagevillagersTabButtonIconPressed = villagersTabButtonIconPressed.getImage(); // transform it
             Image newimgvillagersTabButtonIconPressed = imagevillagersTabButtonIconPressed.getScaledInstance((int) (148*1.5), (int) (131*1.5),  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
             villagersTabButtonIconPressed = new ImageIcon(newimgvillagersTabButtonIconPressed);  // transform it back
