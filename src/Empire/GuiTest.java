1bcd4343c1f8d93da85e8017bf80ebe2334a242c
adijian <shoogeh.jian@gmail.com>
2021-12-11 21:49:26 +0200

    t

--- src/Empire/GuiTest.java
+++ src/Empire/GuiTest.java
@@ -1,2 +1,29 @@
-package Empire;public class GuiTest {
+package Empire;
+
+import javax.swing.*;
+
+public class GuiTest {
+    private JPanel Scientists;
+    private JPanel Gap1;
+    private JPanel Potatoes;
+    private JPanel Land;
+    private JPanel Ore;
+    private JPanel Weapons;
+    private JPanel Medicine;
+    private JPanel SettingsButton;
+    private JPanel FormPanel;
+    private JPanel Headers;
+    private JPanel BelowHeadersPanel;
+    private JPanel HeaderLeft;
+    private JPanel HeaderMiddle;
+    private JPanel HeaderRight;
+    private JPanel HeaderLeftFirst;
+    private JPanel HeaderLeftSecond;
+    private JPanel HeaderLeftThird;
+    private JPanel HeaderLeftFourth;
+    private JPanel HeaderMiddleFirst;
+    private JPanel HeaderMiddleSecond;
+    private JPanel HeaderMiddleThird;
+    private JPanel HeaderRightFirst;
+    private JLabel iconScientists;
 }
