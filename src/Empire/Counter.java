6fab6683fe1884260cbf453d67f59e0bfb839e18
adijian <shoogeh.jian@gmail.com>
2021-12-18 10:05:13 +0200

    e

--- src/Empire/Counter.java
+++ src/Empire/Counter.java
@@ -6,7 +6,6 @@ public class Counter {
     public double farmersGatherBatch = 7.5;
     public double farmersTimeModifier = 0.1;
     public double gatheringSpeedmodifier = 0.50;
-    public static Thread t1;
 
     public void setHoney(int honeyNewNumber) {
         honey += honeyNewNumber;
@@ -20,7 +19,7 @@ public class Counter {
     private class RunnableImpl implements Runnable {
 
         public void run() {
-            while (Counter.t1 != null) {
+            while (true) {
                 try {
                     setHoney((int) (EmpireGame.Engine.villagersFarmingArray.size() * farmersGatherBatch * gatheringSpeedmodifier));
                     System.out.println(
